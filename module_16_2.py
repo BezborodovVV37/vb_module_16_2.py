49efd40405cfe2f497c9f5472ef21f3d9cd721cc
BezborodovVV37 <blood371993@gmail.com>
2024-12-23 11:07:37 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -3,23 +3,23 @@ from typing import Annotated
 
 app = FastAPI()
 
-#Для запуска сервера, в терминале используйте команду :  uvicorn module_16_2:app --reload
+# Для запуска сервера, в терминале используйте команду: uvicorn module_16_2:app --reload
 
 @app.get('/')
-async def wetcome() -> dict:
-    return {'message': 'Главная страница'}
+async def welcome() -> str:
+    return "Главная страница"
 
 @app.get('/user/admin')
-async def admin() -> dict:
-    return {'message': 'Вы вошли как администратор'}
-
+async def admin() -> str:
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def id_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=47)]) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def id_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=47)]) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user/{username}/{age}')
 async def info(
-        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser ')],
+        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}.'
+
